d0da2464dbeba81fed9975330508d74bc6bc6e8d
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-19 00:14:25 +0530

    Add indexjs to have tabbed views

--- src/js/views/base.js
+++
@@ -1,6 +0,0 @@
-export default {
-	content: document.querySelector('#content'),
-	header: document.querySelector('.header'),
-	menu: document.querySelector('.section-menu'),
-	showcase: document.querySelector('.section-showcase')
-};
